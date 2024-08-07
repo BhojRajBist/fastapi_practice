941bd6ba047fe3fe61ca010381d26da4a30731e1
Bhoj Raj Bist <bhojbist1@gmail.com>
2024-08-07 22:25:51 +0545

    postgres

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
+from models import Base  # Import the Base class from models
+from database import engine  # Import the engine from database
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup_event():
+    # Create the database tables
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
-
 @app.get("/name/{name_id}")
-async def name(name_id):
-    return {f"Hello {name_id}"}
+async def name(name_id: int):
+    return {"message": f"Hello {name_id}"}
